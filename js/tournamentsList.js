b2dbfa63a2c7cbde10f79e051dd661293dee2c67
serp0v <serpov.cc@mail.ru>
2023-02-14 19:27:01 +1000

    add adminka page n change parcipants function n add phone adaptive

--- js/tournamentsList.js
+++ js/tournamentsList.js
@@ -3,7 +3,7 @@ const trigger = document.querySelector('#tournament-more').onclick = () => {
     tournament.classList.toggle('tournament-anim');
 };
 
-const participants = document.querySelector('#participants');
-const tournamentButton = document.querySelector('#tournament-button').onclick = () => {
-    participants.classList.toggle('dn');
-};
+// const participants = document.querySelector('#participants');
+// const tournamentButton = document.querySelector('#tournament-button').onclick = () => {
+//     participants.classList.toggle('dn');
+// };
