fa22ce298e81106370d8e0afa38d2ee653531aa4
Dunissimo <SuperDen214@yandex.ru>
2023-02-16 20:29:07 +0400

    refactor auth form

--- js/header.js
+++ js/header.js
@@ -19,15 +19,53 @@ const headerProfile = document.querySelector('#headerProfile');
 //     authorization.classList.remove('dn');
 // };
 
-// const authForm = document.querySelector('#authForm');
-// const inputLogin = document.querySelector('#inputLogin').value;
-// const inputPassword = document.querySelector('#inputPassword').value;
+const authForm = document.querySelector('#authForm');
+const inputLogin = document.querySelector('#inputLogin');
+const inputPassword = document.querySelector('#inputPassword');
 
-// const headerAuthorization = document.querySelector('#headerAuthorization');
-// const authorizationOnclick = document.querySelector('#authorizationOnclick');
+const isVisibleBtn = document.querySelector('.password-visible');
+const isVisibleImg = document.querySelector('.password-visible img');
 
-// authForm.addEventListener('submit', (e) => e.preventDefault())
+const headerAuthorization = document.querySelector('#headerAuthorization');
+const authorizationOnclick = Array.from(document.querySelectorAll('.authorizationOnclick'))
 
-// authorizationOnclick.onclick = () => {
-//     authForm.classList.toggle('dn');
-// };
+authForm.addEventListener('submit', (e) => e.preventDefault())
+
+eventListenerForArray(authorizationOnclick, 'click', (e) => {
+    if (e.target === e.currentTarget) {
+        // authForm.classList.toggle("dn");
+        authForm.classList.toggle('active');
+    }  
+})
+
+isVisibleBtn.addEventListener('click', (e) => {
+    console.log(inputPassword.setAttribute);
+    if (e.target.classList.contains('visible')) {
+        inputPassword.setAttribute('type', 'text')
+        e.target.classList.remove('visible');
+        isVisibleImg.setAttribute('src', '/multimedia/hidden.svg')
+
+        // changeInputVisibility(true, inputPassword);
+    } else {
+        inputPassword.setAttribute('type', 'password')
+        e.target.classList.add('visible');
+        isVisibleImg.setAttribute('src', '/multimedia/visible.svg')
+
+        // changeInputVisibility(false, inputPassword);
+    }
+})
+
+
+function changeInputVisibility(isVisible, input) {
+    if (isVisible) {
+        input.setAttibute('type', 'text')
+    } else {
+        input.setAttibute('type', 'password')
+    }
+}
+
+function eventListenerForArray (arr, eventType, cb) {
+    arr.forEach(item => {
+        item.addEventListener(eventType, (e) => cb(e))
+    })
+}
