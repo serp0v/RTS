6854c130e4fb335c918ed996bee6a320903b1b1e
serp0v <serpov.cc@mail.ru>
2023-02-17 19:24:19 +1000

    add adaptive design for tournament block v.1

--- js/header.js
+++ js/header.js
@@ -19,39 +19,39 @@ const headerProfile = document.querySelector('#headerProfile');
 //     authorization.classList.remove('dn');
 // };
 
-const authForm = document.querySelector('#authForm');
-const inputLogin = document.querySelector('#inputLogin');
-const inputPassword = document.querySelector('#inputPassword');
+// const authForm = document.querySelector('#authForm');
+// const inputLogin = document.querySelector('#inputLogin');
+// const inputPassword = document.querySelector('#inputPassword');
 
-const isVisibleBtn = document.querySelector('.password-visible');
-const isVisibleImg = document.querySelector('.password-visible img');
+// const isVisibleBtn = document.querySelector('.password-visible');
+// const isVisibleImg = document.querySelector('.password-visible img');
 
-const headerAuthorization = document.querySelector('#headerAuthorization');
-const authorizationOnclick = Array.from(document.querySelectorAll('.authorizationOnclick'))
+// const headerAuthorization = document.querySelector('#headerAuthorization');
+// const authorizationOnclick = Array.from(document.querySelectorAll('.authorizationOnclick'))
 
-authForm.addEventListener('submit', (e) => e.preventDefault())
+// authForm.addEventListener('submit', (e) => e.preventDefault())
 
-eventListenerForArray(authorizationOnclick, 'click', (e) => {
-    if (e.target === e.currentTarget) {
-        authForm.classList.toggle('active');
-    }  
-})
+// eventListenerForArray(authorizationOnclick, 'click', (e) => {
+//     if (e.target === e.currentTarget) {
+//         authForm.classList.toggle('active');
+//     }  
+// })
 
-isVisibleBtn.addEventListener('click', (e) => {
-    console.log(inputPassword.setAttribute);
-    if (e.target.classList.contains('visible')) {
-        inputPassword.setAttribute('type', 'text')
-        e.target.classList.remove('visible');
-        isVisibleImg.setAttribute('src', '/multimedia/hidden.svg')
-    } else {
-        inputPassword.setAttribute('type', 'password')
-        e.target.classList.add('visible');
-        isVisibleImg.setAttribute('src', '/multimedia/visible.svg')
-    }
-})
+// isVisibleBtn.addEventListener('click', (e) => {
+//     console.log(inputPassword.setAttribute);
+//     if (e.target.classList.contains('visible')) {
+//         inputPassword.setAttribute('type', 'text')
+//         e.target.classList.remove('visible');
+//         isVisibleImg.setAttribute('src', '/multimedia/hidden.svg')
+//     } else {
+//         inputPassword.setAttribute('type', 'password')
+//         e.target.classList.add('visible');
+//         isVisibleImg.setAttribute('src', '/multimedia/visible.svg')
+//     }
+// })
 
-function eventListenerForArray (arr, eventType, cb) {
-    arr.forEach(item => {
-        item.addEventListener(eventType, (e) => cb(e))
-    })
-}
+// function eventListenerForArray (arr, eventType, cb) {
+//     arr.forEach(item => {
+//         item.addEventListener(eventType, (e) => cb(e))
+//     })
+// }
