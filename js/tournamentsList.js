e7e4004c3dcc22c8bad3946535000d717090bde4
Dunissimo <SuperDen214@yandex.ru>
2023-02-18 19:17:23 +0400

    moving to react

--- js/tournamentsList.js
+++
@@ -1,9 +0,0 @@
-const tournament = document.querySelector('#tournament-more');
-const trigger = document.querySelector('#tournament-more').onclick = () => {
-    tournament.classList.toggle('tournament-anim');
-};
-
-// const participants = document.querySelector('#participants');
-// const tournamentButton = document.querySelector('#tournament-button').onclick = () => {
-//     participants.classList.toggle('dn');
-// };
