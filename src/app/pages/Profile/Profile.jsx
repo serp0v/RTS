1cb3f045d27b985b5a29ae28679452a4ba59534a
Dunissimo <SuperDen214@yandex.ru>
2023-02-19 12:22:45 +0400

    make profile page

--- src/app/pages/Profile/Profile.jsx
+++ src/app/pages/Profile/Profile.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import Match from "../../components/Match/Match";
+import UserProfile from "../../components/UserProfile/UserProfile";
+
+import "./Profile.css";
+
+const matches = [
+  {
+    firstPlayer: {
+      nickname: "Iamia",
+      type: "HR/VR",
+    },
+    secondPlayer: {
+      nickname: "Mazu",
+      type: "HR/VR",
+    },
+    score: "1:2",
+    id: Math.random() * 100,
+  },
+  {
+    firstPlayer: {
+      nickname: "Darathl",
+      type: "HR/VR",
+    },
+    secondPlayer: {
+      nickname: "Nnanedel",
+      type: "HR/VR",
+    },
+    score: "3:1",
+    id: Math.random() * 100,
+  },
+  {
+    firstPlayer: {
+      nickname: "Siuse",
+      type: "HR/VR",
+    },
+    secondPlayer: {
+      nickname: "Comanaiam",
+      type: "HR/VR",
+    },
+    score: "1:3",
+    id: Math.random() * 100,
+  },
+  {
+    firstPlayer: {
+      nickname: "Xananet",
+      type: "HR/VR",
+    },
+    secondPlayer: {
+      nickname: "Denone",
+      type: "HR/VR",
+    },
+    score: "2:2",
+    id: Math.random() * 100,
+  },
+];
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <section className="main-container">
+      <UserProfile />
+      <div className="matches-list">
+        {matches.map((match) => (
+          <Match key={match.id} data={match} />
+        ))}
+      </div>
+    </section>
   );
 };
 
